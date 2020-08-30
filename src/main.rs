020b296185620d5cf00ca4962bc6604bdad5c664
cdunster <cdunster@users.noreply.github.com>
2020-08-30 15:56:43 +0200

    Move common stuff into lib.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-extern crate rlibc;
-
-mod serial;
-mod vga_buffer;
+use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -30,49 +26,8 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    unsafe {
-        let mut port = x86_64::instructions::port::Port::new(0xF4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]");
-    serial_println!("Error: {}", info);
-    exit_qemu(QemuExitCode::Failed);
-
-    loop {}
+    rust_os::test_panic_handler(info);
 }
