cc3395e1b577c90b726b38c253aff7e7eb9d1eb7
cdunster <cdunster@users.noreply.github.com>
2020-08-30 10:55:03 +0200

    Add simple test runner with example test case.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -12,6 +15,9 @@ pub extern "C" fn _start() -> ! {
     println!("Hello, {}!", "World");
     println!("Another line!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -20,4 +26,19 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
